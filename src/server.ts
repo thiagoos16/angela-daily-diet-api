7f5fdb690da06415acfe7e1418608a168f971186
Thiago Oliveira <Thiago.Oliveira2@fit-tecnologia.org.br>
2024-02-13 14:17:44 -0400

    add fastify cookie

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
 import { env } from './env'
 import { usersRoutes } from './routes/users'
+import cookie from '@fastify/cookie'
 
 const app = fastify()
 
+app.register(cookie)
 app.register(usersRoutes, {
   prefix: 'users'
 })
